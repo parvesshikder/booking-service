f18fcc8eb5a42fd49d0660d746e06f37eb6b4ba4
Md Parves Shikder <parvesshikderewu.bd@gmail.com>
2026-05-03 21:57:43 +0300

    Initialize booking service with Maven setup, application structure, and basic configuration

---
+++ src/main/java/ee/ut/eventticketing/booking_service/BookingServiceApplication.java
@@ -0,0 +1,13 @@
+package ee.ut.eventticketing.booking_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingServiceApplication.class, args);
+	}
+
+}
